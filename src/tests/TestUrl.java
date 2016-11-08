578e0c535e7cbae071c31ad620c534de83676754
Pavel <rabo4i79@orange.fr>
2016-11-08 17:25:34 +0100

    test

--- src/tests/TestUrl.java
+++ src/tests/TestUrl.java
@@ -5,7 +5,7 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class TestUrl {
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		try {
 			URL url = new URL("https", "docs.oracle.com", "/javase/specs/jvms/se8/jvms8.pdf");
 			System.out.println(url.toString());
